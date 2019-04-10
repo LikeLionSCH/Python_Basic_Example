d97c00933deef5d76920f7c0ef89ac1746f3b171
alstn2468 <alstn2468_@naver.com>
2019-04-10 14:16:58 +0900

    Add using function quiz

--- quiz.py
+++ quiz.py
@@ -48,3 +48,24 @@ arr = [['' if x % 2 != y % 2 else str(x + 1) for x in range(N)] for y in range(N
 
 for el in arr:
     print(' '.join(el))
+
+
+'''
+Function, Loop, Condition
+피보나치 수열을 출력하는 함수를 구현하세요.
+피보나치 수열은 1, 1, 2, 3, 5, 8, 13, ... 과 같이
+첫째 및 둘째 항이 1이며 그 뒤의 모든 항은 바로 앞 두 항의 합인 수열이다.
+함수의 인자값은 정수형이며 그 정수보다 작은 모든 피보나치 수열의 수를 출력한다.
+'''
+
+def loop_fibonacci(num):
+    before, current = 1, 1
+
+    print(before, end=" ")
+
+    while current <= num:
+        print(current, end=" ")
+
+        before, current = current, before + current
+
+loop_fibonacci(100)
