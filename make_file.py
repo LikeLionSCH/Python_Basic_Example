5f0e0156df7162e877cddbd27530ea966486264a
alstn2468 <alstn2468_@naver.com>
2019-04-11 18:09:47 +0900

    Fix make_file.py

--- make_file.py
+++ make_file.py
@@ -3,6 +3,15 @@ import os
 dir = input("Enter the foler name : ")
 num = 15
 
+try:
+    if not(os.path.isdir(dir)):
+        os.makedirs(os.path.join(dir))
+
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        print("Failed to create directory!!!!!")
+        raise
+
 os.chdir(os.getcwd() + "/" + dir)
 
 for i in range(1, num + 1):
