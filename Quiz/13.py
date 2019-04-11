4cca6cfa6abb6378dc80a11d73c6b7d48c8fd2e7
alstn2468 <alstn2468_@naver.com>
2019-04-12 00:01:44 +0900

    Add another solution in 13.py

--- Quiz/13.py
+++ Quiz/13.py
@@ -17,3 +17,11 @@ for i in range(1, N + 1):
                 print(j, end="")
 
     print()
+
+print('\n'.join(''.join(str(i) if i % 2 == j % 2 else ' ' for i in range(1, N+1))
+                for j in range(1, N+1)))
+
+arr = [[' ' if x % 2 != y % 2 else str(x + 1) for x in range(N)] for y in range(N)]
+
+for el in arr:
+    print(' '.join(el))
